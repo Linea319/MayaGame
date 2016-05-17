f0028f65f563ad5a2e4870531e1ef6ff6da0481f
Linea319 <acyurio@yahoo.co.jp>
2016-05-18 01:46:58 +0900

    カメラがルートの回転と同期するように

---
+++ MayaGame/Assets/script/CamStabilaizer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamStabilaizer : MonoBehaviour {
+    public bool stabX;
+    public bool stabY;
+    public bool stabZ;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Vector3 rotVec = transform.eulerAngles;
+        if (stabX)
+        {
+            rotVec.x = transform.root.eulerAngles.x;
+        }
+        if (stabY)
+        {
+            rotVec.y = transform.root.eulerAngles.y;
+        }
+        if (stabZ)
+        {
+            rotVec.z = transform.root.eulerAngles.z;
+        }
+        Quaternion rot = Quaternion.Euler(rotVec);
+        transform.rotation = rot;
+    }
+}
