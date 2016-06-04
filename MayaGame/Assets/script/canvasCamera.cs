435bf0026996425f480b66a3a93b96d70ef48126
Linea319 <acyurio@yahoo.co.jp>
2016-06-04 15:33:01 +0900

    UIのポスプロ調整

---
+++ MayaGame/Assets/script/canvasCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class canvasCamera : MonoBehaviour {
+    public string cameraName;
+	// Use this for initialization
+	void Start () {
+        if(cameraName != null)
+        {
+            GetComponent<Canvas>().worldCamera = GameObject.Find(cameraName).GetComponent<Camera>();
+        }
+        //GetComponent<Canvas>().worldCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
