016503df19b790c5cac2a252e5d7e40d96e310e9
Linea319 <acyurio@yahoo.co.jp>
2016-06-15 02:27:09 +0900

    IVRCまわり

---
+++ MayaGame/Assets/test/VRblade/lineController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lineController : MonoBehaviour {
+    public LineRenderer line;
+    public Transform[] positions;
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3[] newPos = new Vector3[positions.Length];
+        for(int i = 0; i < newPos.Length; i++)
+        {
+            newPos[i] = positions[i].position;
+        }
+        line.SetPositions(newPos);
+    }
+}
