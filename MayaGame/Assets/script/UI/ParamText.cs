4dafd586ead50bcfbd2b10a419f8cb7b0e0dd71a
Linea319 <acyurio@yahoo.co.jp>
2016-08-28 03:18:32 +0900

    ロードアウトのパラメータ表示

---
+++ MayaGame/Assets/script/UI/ParamText.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ParamText : MonoBehaviour {
+    public Text paramText;
+    public Transform ParamParent;
+    public GameObject GunPrefab;
+    public GameObject MeleePrefab;
+    GameObject cTextObj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetWepParam(string name)
+    {
+        Destroy(cTextObj);
+        GameObject obj = Instantiate(Resources.Load(name, typeof(GameObject))) as GameObject;
+        GunParameter param = obj.GetComponent<Wepon>().pa;
+        DamageParameter damParam = obj.GetComponent<Wepon>().dam;
+        cTextObj = Instantiate(GunPrefab) as GameObject;
+        cTextObj.transform.SetParent(ParamParent,false);
+        cTextObj.GetComponent<ParamTextSetter>().SetGunParam(obj.name, param, damParam);
+        Destroy(obj);
+    }
+
+    public void SetMeleeParam(string name)
+    {
+        Destroy(cTextObj);
+        GameObject obj = Instantiate(Resources.Load(name, typeof(GameObject))) as GameObject;
+        MeleeParameter param = obj.GetComponent<Melee_Wepon>().pa;
+        DamageParameter damParam = obj.GetComponent<Melee_Wepon>().dam;
+        cTextObj = Instantiate(MeleePrefab) as GameObject;
+        cTextObj.transform.SetParent(ParamParent,false);
+        cTextObj.GetComponent<ParamTextSetter>().SetMeleeParam(obj.name, param, damParam);
+        Destroy(obj);
+    }
+}
---
+++ MayaGame/Assets/script/UI/ParamTextSetter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ParamTextSetter : MonoBehaviour {
+    public Text[] texts;
+	
+    public void SetGunParam(string name,GunParameter param,DamageParameter dam)
+    {
+        texts[0].text = "NAME:"+name.Replace("(Clone)","");
+        texts[1].text = "SHOCK:" + ((int)dam.shock).ToString("d3");
+        texts[2].text = "PENETRATE:" + ((int)dam.penetration).ToString("d3");
+        texts[3].text = "HEAT:" + ((int)dam.heat).ToString("d3");
+        texts[4].text = "ACU:" + ((int)param.accuracy).ToString("d3");
+        texts[5].text = "STA:" + ((int)param.recoil).ToString("d3");
+        texts[6].text = "RAN:" + ((int)param.range).ToString("d3");
+        texts[7].text = "MOB:" + ((int)param.mobility).ToString("d3");
+        texts[8].text = "REL:" + ((int)param.reload).ToString("d3");
+        texts[9].text = "ROF:" + ((int)param.rate).ToString("d3");
+        texts[10].text = "MAG:" + param.magazine.ToString("d3");
+        texts[11].text = "AMM:" + ((int)param.totalAmmo).ToString("d3");
+    }
+
+    public void SetMeleeParam(string name, MeleeParameter param, DamageParameter dam)
+    {
+        texts[0].text = "NAME:" + name.Replace("(Clone)", "");
+        texts[1].text = "SHOCK:" + ((int)dam.shock).ToString("d3");
+        texts[2].text = "PENETRATE:" + ((int)dam.penetration).ToString("d3");
+        texts[3].text = "HEAT:" + ((int)dam.heat).ToString("d3");
+        texts[4].text = "TIME:" + ((int)param.chargeTime).ToString("d3");
+        texts[5].text = "CHARGE:" + ((int)param.chargeDamageRaate).ToString("d3");
+        texts[6].text = "STAMINA:" + ((int)param.useStamina).ToString("d3");
+        texts[7].text = "MOB:" + ((int)param.mobility).ToString("d3");
+        texts[8].text = "RAN:" + ((int)param.range).ToString("d3");
+    }
+
+}
