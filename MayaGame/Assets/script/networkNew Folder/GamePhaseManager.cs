9fdda269c89dd536cae49b8c4937f0af96dcfa6d
Linea319 <acyurio@yahoo.co.jp>
2016-09-02 01:54:28 +0900

    ゲーム進行マネージャー雛形

---
+++ MayaGame/Assets/script/networkNew Folder/GamePhaseManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using Prototype.NetworkLobby;
+using System.Collections;
+
+public class GamePhaseManager : NetworkBehaviour {
+    public Transform playerSpawn;
+    public Phase[] phase;
+    public NetworkLobbyManager netMng;
+    float spawnRange = 10f;
+    int phaseCount = 0;
+
+
+	// Use this for initialization
+    [Server]
+	void Start () {
+        //NetworkManager.RegisterStartPosition(playerSpawn);
+        netMng = FindObjectOfType<NetworkLobbyManager>();
+	}
+	
+	// Update is called once per frame
+    [Server]
+	void Update () {
+	
+	}
+
+    [Server]
+    public void NextPhase()
+    { phaseCount++;
+        if(phaseCount >= phase.Length)
+        {
+            StartCoroutine( Clear());
+        }
+    }
+
+    [Server]
+    public IEnumerator Clear()
+    {
+        yield return new WaitForSeconds(2.0f); 
+        netMng.SendReturnToLobby();
+    }
+}
---
+++ MayaGame/Assets/script/networkNew Folder/Phase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Phase : MonoBehaviour {
+    GamePhaseManager manager;
+    public 
+    bool clearFlag;
+    // Use this for initialization
+    void Start() {
+        manager = GetComponent<GamePhaseManager>();
+        StartPhasae();
+    }
+
+    public virtual void StartPhasae()
+    {
+        ClearPhase();
+    }
+
+    public virtual void ClearPhase()
+    {
+        manager.NextPhase();
+        this.enabled = false;
+    }
+}
