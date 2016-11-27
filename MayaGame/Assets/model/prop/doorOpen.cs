23f0ef54d7bacb019af2be0c1ef6a48f6b0f0678
Linea319 <acyurio@yahoo.co.jp>
2016-11-28 01:58:58 +0900

    ドア2＋ドアのロック解除待ち追加

--- MayaGame/Assets/model/prop/doorOpen.cs
+++ MayaGame/Assets/model/prop/doorOpen.cs
@@ -5,8 +5,13 @@ using System.Collections;
 public class doorOpen : NetworkBehaviour {
     [SyncVar]
     bool open = false;
+    [SyncVar]
+    bool openStart = false;
     Collider mycol;
     public Animator anim;
+    public float openTime = 0f;
+    float timer = 0;
+    public GameObject openObject;
 	// Use this for initialization
 	void Start () {
         mycol = GetComponent<Collider>();
@@ -21,8 +26,17 @@ public class doorOpen : NetworkBehaviour {
     [Command]
     void CmdOpen()
     {
-        open = true;
-        RpcOpen();
+        if (openTime <= 0)
+        {
+            open = true;
+            RpcOpen();
+        }
+        else
+        {
+            timer = Time.time + openTime;
+            openStart = true;
+            RpcOpenStart();
+        }
     }
     [ClientRpc]
     void RpcOpen()
@@ -30,11 +44,28 @@ public class doorOpen : NetworkBehaviour {
         mycol.enabled = false;
     }
 
-	// Update is called once per frame
-	void Update () {
+    [ClientRpc]
+    void RpcOpenStart()
+    {
+        if(openObject != null)
+        {
+            openObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (isClient)
         {
             anim.SetBool("open", open);
+        }
+        if (isServer)
+        {
+            if (openStart && Time.time > timer)
+            {
+                open = true;
+                RpcOpen();
+            }
         }
 	}
 }
